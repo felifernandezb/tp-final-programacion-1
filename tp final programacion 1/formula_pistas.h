f252902ffd17671acafe2fac940031fe96d9ff30
Ludwig Qüide <juanjosesm292007@gmail.com>
2026-06-12 06:15:25 -0300

    Función de buscarPistaPorID, generarIdPista y registrarPista creadas.
    Tengo un par de dudas que consultar al respecto...

--- tp final programacion 1/formula_pistas.h
+++ tp final programacion 1/formula_pistas.h
@@ -1,6 +1,18 @@
 #ifndef FORMULA_PISTAS_H_INCLUDED
 #define FORMULA_PISTAS_H_INCLUDED
 
+/// FUNCIONES DE CARGA, EDICIÓN Y ELIMINACIÓN DE PISTAS
+int buscarPistaPorId (stPista pistas[], int cant, int id);
+int generarIDPista (stPista pistas[], int cant);
+int registrarPista (stPista pistas[], int *cant, stPista nuevo);
+int modificarPista ();
+int eliminarPista ();
 
+/// FUNCIONES DE ORDEN Y MUESTRA
+void ordenarPistasAlfabeticamente ();
+void listarPistas ();
+
+void guardarPista ();
+void cargarPista ();
 
 #endif // FORMULA_PISTAS_H_INCLUDED
