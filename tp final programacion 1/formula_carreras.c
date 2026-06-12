d3e3cc91d906d6ee29604efc4a4c6c1354a982f4
franco390 <francofanelli580@gmail.com>
2026-06-12 16:31:51 -0300

    agregue las funciones de carrera

---
+++ tp final programacion 1/formula_carreras.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <string.h>
+#include "formula.h"
+
+// =================
+// FUNCIONES CARRERAS
+// =================
+
+int buscarCarreraPorId (stCarrera carrera[], int cant, int id)
+{
+    for (int i = 0; i < cant; i++)
+    {
+        if (id == carrera[i].id)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int generarIdCarrera(stCarrera carrera[], int cant)
+{
+    int maxId = 0;
+    for (int i = 0; i < cant; i++)
+    {
+        if (carrera[i].id > maxId)
+        {
+            maxId = carrera[i].id;
+        }
+    }
+    return maxId + 1;
+}
+
+int registrarCarrera(stCarrera carrera[], int *cant, stCarrera nuevo)
+{
+
+    if (*cant >= MAX_CARRERAS)
+    {
+        return 0;
+    }
+
+    carrera[*cant] = nuevo;
+    (*cant)++;
+
+    guardarCarrera(carrera, *cant);
+
+    return 1;
+}
+
+void guardarCarreras(stCarrera carrera[], int cant)
+{
+    FILE *archivo = fopen("carreras.dat", "wb");
+
+    if (archivo == NULL)
+    {
+        printf("Error al abrir el archivo de carreras.\n");
+        return;
+    }
+
+    fwrite(&cant, sizeof(int), 1, archivo);
+
+    for (int i = 0; i < cant; i++)
+    {
+        fwrite(&carrera[i], sizeof(stCarrera), 1, archivo);
+    }
+
+    fclose(archivo);
+}
+
+void cargarCarreras(stCarrera carrera[], int *cant)
+{
+    FILE *archivo = fopen("carreras.dat", "rb");
+
+    if (archivo == NULL)
+    {
+        *cant = 0;
+        return;
+    }
+
+    fread(cant, sizeof(int), 1, archivo);
+
+    for (int i = 0; i < *cant; i++)
+    {
+        fread(&carrera[i], sizeof(stCarrera), 1, archivo);
+    }
+
+    fclose(archivo);
+}
+
+void listarCarreras(stCarrera carrera[], int cant, stPista pista[], int cantPistas)
+{
+    if (cant == 0)
+    {
+        printf("No hay carreras registradas.\n");
+        return;
+    }
+
+    printf("%-6s %-30s %-20s %-10s %-10s %-10s\n", "ID", "Fecha", "Pista", "1ro", "2ro", "3ro");
+    printf("--------------------------------------------------------------------\n");
+
+    for (int i = 0; i < cant; i++)
+    {
+        int posEsc = buscarEscuderiaPorId(escuderias, cantEscuderias, pilotos[i].idEscuderia);
+
+        printf("%-6d %-30s %-20s %-10d %-10s\n %-10s",
+            pilotos[i].id,
+            pilotos[i].nombre,
+            escuderias[posEsc].marca,
+            pilotos[i].puntaje,
+            pilotos[i].categoria == 1 ? "F1" : "F2");
+    }
+}
--- tp final programacion 1/formula_carreras.h
+++ tp final programacion 1/formula_carreras.h
@@ -1,6 +1,19 @@
 #ifndef FORMULA_CARRERAS_H_INCLUDED
 #define FORMULA_CARRERAS_H_INCLUDED
 
+#include "formula.h"
+
+// ========================
+//   CARRERA
+// ========================
+
+int buscarCarreraPorId (stCarrera carrera[], int cant, int id);
+int generarIdCarrera(stCarrera carrera[], int cant);
+int registrarCarrera(stCarrera carrera[], int *cant, stCarrera nuevo);
+void guardarCarreras(stCarrera carrera[], int cant);
+void cargarCarreras(stCarrera carrera[], int *cant);
+void listarCarreras(stCarrera carrera[], int cant, stPista pista[], int cantPistas);
+
 
 
 #endif // FORMULA_CARRERAS_H_INCLUDED
